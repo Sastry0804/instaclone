509ac530ca53ea3df4827f2e56fa3db7c3a540c4
Sastry0804 <64729748+Sastry0804@users.noreply.github.com>
2020-11-15 21:30:22 +0530

    Update firebase.js
    Removed credentials

--- src/firebase.js
+++ src/firebase.js
@@ -1,14 +1,14 @@
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 import firebase from "firebase";
 const firebaseConfig = {
-  apiKey: "AIzaSyA57DjPlsNfvMo9K-G5v4zcjm26zIprv9Q",
-  authDomain: "insta-clone-react-ca1d1.firebaseapp.com",
-  databaseURL: "https://insta-clone-react-ca1d1.firebaseio.com",
-  projectId: "insta-clone-react-ca1d1",
-  storageBucket: "insta-clone-react-ca1d1.appspot.com",
-  messagingSenderId: "378775932134",
-  appId: "1:378775932134:web:72bfb327ad335c97f0bf27",
-  measurementId: "G-6LWD68PNSC",
+  apiKey: "API KEY",
+  authDomain: "DOMAIN",
+  databaseURL: " DB URL ",
+  projectId: "Project ID",
+  storageBucket: " STORAGE ",
+  messagingSenderId: "",
+  appId: "",
+  measurementId: "",
 };
 
 const firebaseApp = firebase.initializeApp(firebaseConfig);
